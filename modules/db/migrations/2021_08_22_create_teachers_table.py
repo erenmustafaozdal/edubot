915cb6e15867a4551431bafc75e49a4492c12897
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2021-08-25 21:13:06 +0300

    Öğretmen migration düzenlendi

--- modules/db/migrations/2021_08_22_create_teachers_table.py
+++ modules/db/migrations/2021_08_22_create_teachers_table.py
@@ -3,23 +3,31 @@ class CreateTeachersTable:
     @staticmethod
     def up():
         return [
-            'CREATE TABLE IF NOT EXISTS "teachers" ('
-                '"id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
-                '"first_name" VARCHAR(191) NOT NULL,'
-                '"last_name" VARCHAR(191) NOT NULL,'
-                '"branch" VARCHAR(191) NOT NULL,'
-                '"login_type" VARCHAR(10),'
-                '"mebbis_user_name" VARCHAR(11),'
-                '"mebbis_password" VARCHAR(191),'
-                '"edevlet_tc" VARCHAR(11),'
-                '"edevlet_password" VARCHAR(191),'
-                '"is_active" BOOLEAN(1) NOT NULL DEFAULT 0,'
-                '"created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,'
-                '"updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,'
+            # TABLO OLUŞTUR
+            'CREATE TABLE IF NOT EXISTS teachers ('
+                'id  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                'first_name VARCHAR(191) NOT NULL,'
+                'last_name VARCHAR(191) NOT NULL,'
+                'branch VARCHAR(191) NOT NULL,'
+                'login_type VARCHAR(10),'
+                'mebbis_username VARCHAR(11),'
+                'mebbis_password VARCHAR(191),'
+                'edevlet_tc VARCHAR(11),'
+                'edevlet_password VARCHAR(191),'
+                'is_active BOOLEAN(1) NOT NULL DEFAULT 1,'
+                'created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,'
+                'updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,'
                 # is_active alanına sadece 0 ve 1 değerlerini kabul ediyoruz
-                'CONSTRAINT "teachers.is_active" CHECK ("is_active" IN (0,1)),'
+                'CONSTRAINT teachers_is_active CHECK (is_active IN (0,1)),'
                 # login_type alanına sadece "mebbis" ve "edevlet" değerlerini kabul ediyoruz
-                'CONSTRAINT "teachers.login_type" CHECK ("login_type" IN ("mebbis", "edevlet"))'
+                'CONSTRAINT teachers_login_type CHECK (login_type IN ("mebbis", "edevlet"))'
             ');',
-            'CREATE UNIQUE INDEX IF NOT EXISTS "teachers.PRIMARY" ON "teachers" ("id" ASC);'
+            # INDEX OLUŞTUR
+            'CREATE UNIQUE INDEX IF NOT EXISTS teachers_id ON teachers (id ASC);',
+            # GÜNCELLENDİĞİ TARİH İÇİN TRIGGER OLUŞTUR
+            'CREATE TRIGGER IF NOT EXISTS teachers_updated_at '
+            'AFTER UPDATE OF first_name, last_name, branch, login_type, mebbis_username, mebbis_password, edevlet_tc, edevlet_password ON teachers '
+            'BEGIN '
+                'UPDATE teachers SET updated_at=CURRENT_TIMESTAMP WHERE id=NEW.id; '
+            'END'
         ]
